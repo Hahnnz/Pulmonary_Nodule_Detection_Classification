83e03dc91ec4e133293665b497bfb3a2506f54d6
Hahnnz <lphion96@naver.com>
2018-07-01 23:20:37 +0900

    modified config

--- scripts/config.py
+++ scripts/config.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 import cv2
 import tensorflow as tf
+from copy import copy
 from tqdm import tqdm
 
 project_root = os.getcwd()
@@ -12,4 +13,12 @@ train_roi_root = project_root + "/dataset/train/labels/*"
 val_img_root = project_root + "/dataset/val/images/*"
 val_roi_root = project_root + "/dataset/val/labels/*"
 
-test_img_root = project_root + "/dataset/test/images/*"
+train_img_root_b = project_root + "/dataset/train/images/benign"
+train_roi_root_b = project_root + "/dataset/train/labels/benign"
+train_img_root_m = project_root + "/dataset/train/images/malignant"
+train_roi_root_m = project_root + "/dataset/train/labels/malignant"
+
+val_img_root_b = project_root + "/dataset/val/images/benign"
+val_roi_root_b = project_root + "/dataset/val/labels/benign"
+val_img_root_m = project_root + "/dataset/val/images/malignant"
+val_roi_root_m = project_root + "/dataset/val/labels/malignant"
