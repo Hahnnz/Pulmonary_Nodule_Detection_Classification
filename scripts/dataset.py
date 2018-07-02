823e73521e437c0d5bdcac17381c1e3c74c53e60
Hahnnz <lphion96@naver.com>
2018-07-02 17:10:20 +0900

    added dataset script with lmdb

--- scripts/dataset.py
+++ scripts/dataset.py
@@ -6,12 +6,15 @@ from multiprocessing import Pool
 
 
 class load():
-    def __init__(self, Class, img_root, roi_root, 
+    def __init__(self, Class, Phase, img_root, roi_root,
                  Rotate=False, theta_set={90},Fliplr=False, Resize=False, Shuffle=False, num_process=4):
         if Class.lower() not in ['benign','malignant']:
             raise ValueError('Class must be "benign" or "malignant".')
+        if Phase.lower() not in ['train','test']:
+            raise ValueError('Phase must be "train" or "test".')
         
         self.Class = Class.lower()
+        self.Phase = Phase.lower()
         
         img_path = explore_dir(img_root)[np.newaxis,:,:]
         roi_path = explore_dir(roi_root)[np.newaxis,:,:]
@@ -54,7 +57,7 @@ class load():
         self.n_roi=list()
         
         with tqdm(total=self.dataset.shape[1]) as pbar:
-            pbar.set_description("[Class : "+self.Class+"]")
+            pbar.set_description("[Phase : "+self.Phase+"] / [Class : "+self.Class+"]")
             for i in range(self.dataset.shape[1]):
                 img = self.dataset[0,i]
                 roi = self.dataset[1,i]
@@ -74,5 +77,66 @@ class load():
     def _im_and_roi_read(self, paths):
         return cv2.imread(paths[1,0]), cv2.imread(paths[1,1])
     
-#class save():
-    # lmdb saver needed
+class LMDB():
+    def __init__(self, Class, Phase, mode, dataset=None,
+                 caffe_root = './caffe/', lmdb_root='./LMDB/'):
+        if Class.lower() not in ['benign','malignant']:
+            raise ValueError('Class must be "benign" or "malignant".')
+        if Phase.lower() not in ['train','test']:
+            raise ValueError('Phase must be "train" or "test".')
+        if mode.lower() not in ['read','write']:
+            raise ValueError('mode must be "read" or "write".')
+        if mode.lower() == 'write' and dataset == None:
+            raise ValueError('Dataset is required when mode is "write".')
+        
+        self.Class = Class.lower()
+        self.Phase = Phase.lower()
+            
+        if mode == 'write':
+            self.write(dataset, caffe_root, lmdb_root)
+        elif mode == 'read':
+            self.dataset = self.read(lmdb_root)
+            
+    def write(self, dataset, caffe_root, lmdb_root):
+        try:
+            import lmdb
+            import sys
+            sys.path.insert(0, caffe_root + 'python')
+            import caffe
+            from caffe.proto import caffe_pb2
+        except:
+            raise ValueError("Library Error Occurred!")
+            
+        lmdb_env = lmdb.open(lmdb_root+self.Phase+'_'+self.Class+'_lmdb', map_size=int(1e12))
+        # B X C X W X H
+        for i in range(len(dataset[self.Class+'_img'])):
+            datum_img = caffe.io.array_to_datum(dataset[self.Class+'_img'][i].transpose(2,0,1))
+            datum_roi = caffe.io.array_to_datum(dataset[self.Class+'_roi'][i].transpose(2,0,1))
+
+            with lmdb_env.begin(write=True) as lmdb_txn:
+                lmdb_txn.put((self.Class+'_img_'+'{:08}'.format(i+1)).encode('ascii'),datum_img.SerializeToString())
+                lmdb_txn.put((self.Class+'_roi_'+'{:08}'.format(i+1)).encode('ascii'),datum_roi.SerializeToString())
+                
+    def read(self, lmdb_root):
+        try:
+            import lmdb
+        except:
+            raise ValueError("Library Error Occurred!")
+            
+        lmdb_env = lmdb.open(lmdb_root+self.Phase+"_"+self.Class+"_lmdb")
+        lmdb_txn = lmdb_env.begin()
+        lmdb_cursor = lmdb_txn.cursor()
+    
+        img =list()
+        roi = list()
+    
+        n_counter=0
+        with lmdb_env.begin() as lmdb_txn:
+            with lmdb_txn.cursor() as lmdb_cursor:
+                for key, value in lmdb_cursor:  
+                    if(self.Class+'_img' in str(key)):
+                        img.append(np.fromstring(value, dtype=np.uint8)[:12288].reshape(3,64,64).transpose(1,2,0))
+                    if(self.Class+'_roi' in str(key)):
+                        roi.append(np.fromstring(value, dtype=np.uint8)[:12288].reshape(3,64,64).transpose(1,2,0))
+    
+        return {self.Class+'_img': np.array(img), self.Class+'_roi': np.array(roi)}
